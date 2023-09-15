75e02a70549c8dd2c7e3e46b5f1a8f5acd49c52c
ozgeahras <ozgeahras@gmail.com>
2023-09-15 11:50:40 +0300

    added logs

--- netlify/functions/openai.js
+++ netlify/functions/openai.js
@@ -6,33 +6,31 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
+console.log("buraya geldi1");
 
 export async function handler(event) {
-  try {
-    const { text } = JSON.parse(event.body);
-    const response = await openai.createChatCompletion({
-      model: "gpt-4",
-      prompt: `Translate this to turkish.
-      ###
-      outline: ${text}
-      message: 
-      `,
-      max_tokens: 60,
-      presence_penalty: 0,
-      frequency_penalty: 0.3,
-    });
-
-    console.log(response);
-    const reply = response.data.choices[0].message.content;
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ reply }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Something went wrong" }),
-    };
-  }
+  return "test-deneme";
+  // try {
+  //   const { text } = JSON.parse(event.body);
+  //   const response = await openai.createCompletion({
+  //     model: "text-davinci-003",
+  //     prompt: `Translate this to turkish.
+  //     ###
+  //     outline: ${text}
+  //     message:
+  //     `,
+  //     max_tokens: 60,
+  //   });
+  //   console.log(response);
+  //   const reply = response.data.choices[0].message.content;
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify({ reply }),
+  //   };
+  // } catch (error) {
+  //   return {
+  //     statusCode: 500,
+  //     body: JSON.stringify({ error: "Something went wrong" }),
+  //   };
+  // }
 }
